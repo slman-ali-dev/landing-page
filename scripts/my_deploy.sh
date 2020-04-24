b35860839a088ac9b228b2d134012831f0d387e2
Slman Ali <slmano.ali69@gmail.com>
2020-04-24 17:21:55 +0300

    fix my_deploy.sh

--- scripts/my_deploy.sh
+++ scripts/my_deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "changing directory to greenlight"
-cd /var/www/greenlightSSH/greenlight
+cd /var/www/github/quiq
 
 echo "down"
 sudo docker-compose down
 
 echo "rebuild"
-sudo ./scripts/image_build.sh mhara release-v2
+sudo ./scripts/image_build.sh quiq release-v2
 
 echo "up"
 sudo docker-compose up -d
